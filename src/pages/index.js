c8a630d0685e7cab28dbab1a4eef3bcb13630b38
Mohamed Gaber <migabora@gmail.com>
2018-11-27 11:55:29 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Spark UI</h1>
+    <p>instabug design system documentation</p>
+  </Layout>
+)
+
+export default IndexPage
