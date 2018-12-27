bb79513cee416249dfcf509484acc5c0a193238f
Mohamed Gaber <migabora@gmail.com>
2018-12-27 14:56:15 +0200

    update site fav icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
         background_color: '#1f82dc',
         theme_color: '#1f82dc',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon-32x32.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
